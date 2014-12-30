61a65ca9432640d863bc322a17d3e99a93074b37
luoweihua <luoweihua7@gmail.com>
2014-12-30 18:23:22 +0800

    update

--- src/Locality/Services/FileService.cs
+++ src/Locality/Services/FileService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.IO;
+
 namespace Locality
 {
     public class FileService
@@ -14,15 +16,26 @@ namespace Locality
             switch (fileHook.Type)
             {
                 case HookType.File:
-
+                    if (fileHook.Path.EndsWith(fileName))
+                    {
+                        filePath = fileHook.Path;
+                    }
                     break;
                 case HookType.Folder:
-
+                    filePath = FindFile(fileName, fileHook.Path);
                     break;
                 default:
                     break;
             }
 
+            return filePath;
+        }
+
+        public static string FindFile(string fileName, string folderName)
+        {
+            string filePath = null;
+
+            string[] files = Directory.GetFiles(folderName);
 
 
             return filePath;
